381206c18d8c0a09a3015df2312038d29bc69553
David Forgeas <david.forgeas@gmail.com>
2021-02-19 16:37:53 +0000

    Decode a Cell in Rust

--- rvm.rs
+++ rvm.rs
@@ -28,7 +28,7 @@ enum _Instr
     Out(Arg),
     In(Arg),
     Noop,
-    Data(Arg)
+    Data(u16)
 }
 
 enum Cell
@@ -56,4 +56,44 @@ enum Cell
     In,
     Noop,
     Data(u16)
+}
+
+impl Cell
+{
+    fn decode(x: u16)
+    {
+        match x
+        {
+            0 => Cell::Halt,
+            1 => Cell::Set,
+            2 => Cell::Push,
+            3 => Cell::Pop,
+            4 => Cell::Eq,
+            5 => Cell::Gt,
+            6 => Cell::Jmp,
+            7 => Cell::Jt,
+            8 => Cell::Jf,
+            9 => Cell::Add,
+            10=> Cell::Mult,
+            11=> Cell::Mod,
+            12=> Cell::And,
+            13=> Cell::Or,
+            14=> Cell::Not,
+            15=> Cell::Rmem,
+            16=> Cell::Wmem,
+            17=> Cell::Call,
+            18=> Cell::Ret,
+            19=> Cell::Out,
+            20=> Cell::In,
+            21=> Cell::Noop,
+            y => Cell::Data(y)
+        }
+    }
+
+    fn encode(&self)
+    {
+        match self
+        {
+        }
+    }
 }
