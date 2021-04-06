a25c68f67e13671fff1686a30c7e0be7d64e2282
David Forgeas <david.forgeas@gmail.com>
2021-04-06 15:21:45 +0100

    Collect the code in a String, then print in one instruction.

--- rmirror.rs
+++ rmirror.rs
@@ -1,13 +1,11 @@
 use std::io::BufRead;
 fn main() {
     for line in std::io::stdin().lock().lines() {
-        for c in line.unwrap().chars().rev().map(|x| match x {
+        let code = line.unwrap().chars().rev().map(|x| match x {
             'q' => 'p',
             'p' => 'q',
             _ => x
-        }) {
-            print!("{}", c);
-        }
-        println!("");
+        }).collect::<String>();
+        println!("{}", code);
     }
 }
